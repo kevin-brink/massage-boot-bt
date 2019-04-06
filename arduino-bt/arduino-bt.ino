c007a1e351099f4c3dc7cd70b248b0325c89146a
kevin-brink <kbrink170@gmail.com>
2019-04-06 14:54:18 -0500

    finished arduino side for now

--- arduino-bt/arduino-bt.ino
+++ arduino-bt/arduino-bt.ino
@@ -1,9 +1,94 @@
-void setup() {
-  // put your setup code here, to run once:
+#define bt_rx 0     //Receive from BT     (Shouldn't actually need
+#define bt_tx 1     //Transmit to BT       to use these two directly)
+const int NUM = 8;
+const int FIRST = 6;
+int valve[NUM];
 
+void setup() {      // put your setup code here, to run once:
+  setup_bluetooth();
+  for (int i = 0; i < NUM; ++i)
+    valve[i] = FIRST + i;
+  for(int i = 0; i < NUM; ++i) {    //Start with all valves 'false'
+    pinMode(valve[i], OUTPUT);
+    digitalWrite(valve[i], LOW);
+  }
+  pinMode(4, OUTPUT);
+  
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+char seqx = 0;
 
+void loop() {       // put your main code here, to run repeatedly
+  seqx = check_bluetooth(seqx);
+  seqxuence();
+}
+
+void seqxuence() {
+  int timer;
+  switch (seqx) {
+    case '1': {
+      timer = 250;
+      for (int i = 0; i < NUM; ++i) {
+        seqx = check_bluetooth(seqx);
+        delay(timer);
+        digitalWrite(valve[i], HIGH);
+      }
+      digitalWrite(4, HIGH);
+    
+      for (int i = 0; i< NUM; ++i) {
+        seqx = check_bluetooth(seqx);
+        delay(timer);
+        digitalWrite(valve[i], LOW);
+      }
+      digitalWrite(4, LOW);
+    } 
+      Serial.write("Finished 1\n");break;
+    
+    case '2': {
+      timer = 100;
+      for (int i = 0; i < NUM; ++i) {
+        seqx = check_bluetooth(seqx);
+        delay(timer);
+        digitalWrite(valve[i], HIGH);
+        for (int j = i + 1; j < NUM; ++j) {
+          seqx = check_bluetooth(seqx);
+          delay(timer);
+          digitalWrite(valve[j], HIGH);
+        }
+        for (int j = NUM - 1; j > i; --j) {
+          seqx = check_bluetooth(seqx);
+          delay(timer);
+          digitalWrite(valve[j], LOW);
+        }
+      }
+      delay(timer * 5);
+      for (int i = 0; i < NUM; ++i)
+        digitalWrite(valve[i], LOW);
+    }
+      Serial.write("Finished 2\n"); break;
+    
+    case '3': {
+      timer = 1000;
+      for (int i = 0; i < NUM; ++i) {
+        seqx = check_bluetooth(seqx);
+        digitalWrite(valve[i], HIGH);
+        delay(timer);
+        digitalWrite(valve[i], LOW);
+      }
+    }
+      Serial.write("Finished 3\n"); break;
+    
+    default: {
+      timer = 500;
+      for (int i = 0; i < NUM; ++i) 
+        digitalWrite(valve[i], HIGH);
+      seqx = check_bluetooth(seqx);
+      delay(timer);
+      for (int i = 0; i < NUM; ++i)
+        digitalWrite(valve[i], LOW);
+      seqx = check_bluetooth(seqx);
+      delay(timer);
+    }
+      Serial.write("Finished Def\n");
+  }
 }
--- arduino-bt/bluetooth.ino
+++ arduino-bt/bluetooth.ino
@@ -1,1 +1,29 @@
+bool setup_bluetooth() {
+  Serial.begin(9600);
+  Serial.println("Bluetooth now active");
 
+  return 0;
+}
+
+char check_bluetooth(char seq) {
+  if (Serial.available() > 0) {
+    Serial.println("Config opened");
+    Serial.println("Only 1, 2, 3 are valid for now. Any others will result in the default of 0.");
+    Serial.println("Except for X. X is the emergency stop trigger, which kills the sequence thread.");
+    seq = Serial.read();
+    if (seq == 'X')
+      estop_bluetooth();
+    Serial.write("Sequence ");
+    Serial.write(seq);
+    Serial.write(" set. Will start when current sequence finishes.\n");
+  }
+  return seq;
+}
+
+void estop_bluetooth() {
+  Serial.println("\n\n***EMERGENCY STOP INITATED***");
+  Serial.println("Hard-reset device to re-enable.");
+  for (int i = 0; i < NUM; ++i)
+    digitalWrite(valve[i], LOW);
+  while(1);
+}
